07edcf2941c35c7c6cc40a32f0ed22f8f67670ae
sfjeffersonfr <sfjeffersonfr@4da512b3-211c-4bb9-99b0-1bc5b7860a2b>
2012-10-05 18:04:36 +0000

    Fixed problems with DrawString()

--- jgui/binds/directfb/jdfbgraphics.cpp
+++ jgui/binds/directfb/jdfbgraphics.cpp
@@ -1224,36 +1224,14 @@ void DFBGraphics::DrawString(std::string text, int xp, int yp)
 		return;
 	}
 
-	Image *off = Image::CreateImage(_font->GetStringWidth(text), _font->GetAscender() + _font->GetDescender(), JPF_A8, _scale.width, _scale.height);
-
-	off->GetGraphics()->SetFont(_font);
-	off->GetGraphics()->SetColor(_color);
-
-	IDirectFBSurface *fsurface = (IDirectFBSurface *)(off->GetGraphics()->GetNativeSurface());
-
-	fsurface->DrawString(fsurface, text.c_str(), -1, 0, 0, (DFBSurfaceTextFlags)(DSTF_LEFT | DSTF_TOP));
-
-	jblitting_flags_t bf = GetBlittingFlags();
-	jcomposite_flags_t cf = GetCompositeFlags();
-
-	SetCompositeFlags(JCF_NONE);
-	SetBlittingFlags((jblitting_flags_t)(bf | JBF_ALPHACHANNEL | JBF_COLORIZE));
-
-	DrawImage(off, xp, yp);
-
-	SetBlittingFlags(bf);
-	SetCompositeFlags(cf);
-
-	delete off;
-
 	/*
 	if (_radians == 0.0) {
 		int x = SCALE_TO_SCREEN((_translate.x+xp), _screen.width, _scale.width),
 				y = SCALE_TO_SCREEN((_translate.y+yp), _screen.height, _scale.height);
 
 		_surface->DrawString(_surface, text.c_str(), -1, x, y, (DFBSurfaceTextFlags)(DSTF_LEFT | DSTF_TOP));
-	} else {
-		Image *off = Image::CreateImage(_font->GetStringWidth(text), _font->GetAscender() + _font->GetDescender(), JPF_A8, _scale.width, _scale.height);
+	} else */ {
+		Image *off = Image::CreateImage(_font->GetStringWidth(text), _font->GetAscender() + _font->GetDescender(), JPF_ARGB, _scale.width, _scale.height);
 
 		off->GetGraphics()->SetFont(_font);
 		off->GetGraphics()->SetColor(_color);
@@ -1262,17 +1240,13 @@ void DFBGraphics::DrawString(std::string text, int xp, int yp)
 
 		fsurface->DrawString(fsurface, text.c_str(), -1, 0, 0, (DFBSurfaceTextFlags)(DSTF_LEFT | DSTF_TOP));
 
-		jblitting_flags_t t = GetBlittingFlags();
-
-		SetBlittingFlags((jblitting_flags_t)(t | JBF_COLORIZE));
+		jblitting_flags_t bf = GetBlittingFlags();
+		jcomposite_flags_t cf = GetCompositeFlags();
 
 		DrawImage(off, xp, yp);
 
-		SetBlittingFlags(t);
-
 		delete off;
 	}
-	*/
 }
 
 void DFBGraphics::DrawGlyph(int symbol, int xp, int yp)
