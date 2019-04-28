5264d774e9975747ceb3f8ecdf30d1c8576509c7
Jeff Ferr <jeffersonfr@gmail.com>
2019-04-28 14:06:43 -0300

    Minor fixes :)

--- jgui/binds/nanax/nanaxapplication.cpp
+++ jgui/binds/nanax/nanaxapplication.cpp
@@ -48,9 +48,13 @@ static int sg_mouse_x = 0;
 /** \brief */
 static int sg_mouse_y = 0;
 /** \brief */
+static bool sg_fullscreen = false;
+/** \brief */
 static bool sg_quitting = false;
 /** \brief */
 static jgui::jsize_t sg_screen = {0, 0};
+/** \brief */
+static jgui::jregion_t sg_last_bounds = {0, 0, 0, 0};
 /** \brief */
 static std::mutex sg_loop_mutex;
 /** \brief */
@@ -62,196 +66,196 @@ static jcursor_style_t sg_jgui_cursor = JCS_DEFAULT;
 
 static jevent::jkeyevent_symbol_t TranslateToNativeKeySymbol(int symbol, bool capital)
 {
-	switch (symbol) {
-		case 13:
-			return jevent::JKS_ENTER; // jevent::JKS_RETURN;
-		case 8:
-			return jevent::JKS_BACKSPACE;
-		case 9:
-			return jevent::JKS_TAB;
-		// case ALLEGRO_KEY_CANCEL:
-		//	return jevent::JKS_CANCEL;
-		case 27:
-			return jevent::JKS_ESCAPE;
-		case 32:
-			return jevent::JKS_SPACE;
-		// case 39:
-		//	return (capital == true)?jevent::JKS_QUOTATION:jevent::JKS_APOSTHROFE;
-		case 44:   
-			return (capital == true)?jevent::JKS_LESS_THAN_SIGN:jevent::JKS_COMMA;
-		case 45:
-			return (capital == true)?jevent::JKS_UNDERSCORE:jevent::JKS_MINUS_SIGN;
-		case 46:  
-			return (capital == true)?jevent::JKS_GREATER_THAN_SIGN:jevent::JKS_PERIOD;
-		case 47:
-			return (capital == true)?jevent::JKS_QUESTION_MARK:jevent::JKS_SLASH;
-		case 48:     
-			return (capital == true)?jevent::JKS_PARENTHESIS_RIGHT:jevent::JKS_0;
-		case 49:
-			return (capital == true)?jevent::JKS_EXCLAMATION_MARK:jevent::JKS_1;
-		case 50:
-			return (capital == true)?jevent::JKS_AT:jevent::JKS_2;
-		case 51:
-			return (capital == true)?jevent::JKS_NUMBER_SIGN:jevent::JKS_3;
-		case 52:
-			return (capital == true)?jevent::JKS_DOLLAR_SIGN:jevent::JKS_4;
-		case 53:
-			return (capital == true)?jevent::JKS_PERCENT_SIGN:jevent::JKS_5;
-		case 54:
-			return (capital == true)?jevent::JKS_CIRCUMFLEX_ACCENT:jevent::JKS_6;
-		case 55:
-			return (capital == true)?jevent::JKS_AMPERSAND:jevent::JKS_7;
-		case 56:
-			return (capital == true)?jevent::JKS_STAR:jevent::JKS_8;
-		case 57:
-			return (capital == true)?jevent::JKS_PARENTHESIS_LEFT:jevent::JKS_9;
-		case 59:
-			return (capital == true)?jevent::JKS_COLON:jevent::JKS_SEMICOLON;
-		case 61: 
-			return (capital == true)?jevent::JKS_PLUS_SIGN:jevent::JKS_EQUALS_SIGN;
-		case 65:
-			return (capital == true)?jevent::JKS_A:jevent::JKS_a;
-		case 66:
-			return (capital == true)?jevent::JKS_B:jevent::JKS_b;
-		case 67:
-			return (capital == true)?jevent::JKS_C:jevent::JKS_c;
-		case 68:
-			return (capital == true)?jevent::JKS_D:jevent::JKS_d;
-		case 69:
-			return (capital == true)?jevent::JKS_E:jevent::JKS_e;
-		case 70:
-			return (capital == true)?jevent::JKS_F:jevent::JKS_f;
-		case 71:
-			return (capital == true)?jevent::JKS_G:jevent::JKS_g;
-		case 72:
-			return (capital == true)?jevent::JKS_H:jevent::JKS_h;
-		case 73:
-			return (capital == true)?jevent::JKS_I:jevent::JKS_i;
-		case 74:
-			return (capital == true)?jevent::JKS_J:jevent::JKS_j;
-		case 75:
-			return (capital == true)?jevent::JKS_K:jevent::JKS_k;
-		case 76:
-			return (capital == true)?jevent::JKS_L:jevent::JKS_l;
-		case 77:
-			return (capital == true)?jevent::JKS_M:jevent::JKS_m;
-		case 78:
-			return (capital == true)?jevent::JKS_N:jevent::JKS_n;
-		case 79:
-			return (capital == true)?jevent::JKS_O:jevent::JKS_o;
-		case 80:
-			return (capital == true)?jevent::JKS_P:jevent::JKS_p;
-		case 81:
-			return (capital == true)?jevent::JKS_Q:jevent::JKS_q;
-		case 82:
-			return (capital == true)?jevent::JKS_R:jevent::JKS_r;
-		case 83:
-			return (capital == true)?jevent::JKS_S:jevent::JKS_s;
-		case 84:
-			return (capital == true)?jevent::JKS_T:jevent::JKS_t;
-		case 85:
-			return (capital == true)?jevent::JKS_U:jevent::JKS_u;
-		case 86:
-			return (capital == true)?jevent::JKS_V:jevent::JKS_v;
-		case 87:
-			return (capital == true)?jevent::JKS_W:jevent::JKS_w;
-		case 88:
-			return (capital == true)?jevent::JKS_X:jevent::JKS_x;
-		case 89:
-			return (capital == true)?jevent::JKS_Y:jevent::JKS_y;
-		case 90:
-			return (capital == true)?jevent::JKS_Z:jevent::JKS_z;
-		case 91:
-			return (capital == true)?jevent::JKS_CURLY_BRACKET_LEFT:jevent::JKS_SQUARE_BRACKET_LEFT;
-		case 92:   
-			return (capital == true)?jevent::JKS_VERTICAL_BAR:jevent::JKS_BACKSLASH;
-		case 93:
-			return (capital == true)?jevent::JKS_CURLY_BRACKET_RIGHT:jevent::JKS_SQUARE_BRACKET_RIGHT;
-		case 96:
-			return (capital == true)?jevent::JKS_TILDE:jevent::JKS_GRAVE_ACCENT;
-		case 127:
-			return jevent::JKS_DELETE;
-		case 37:
-			return jevent::JKS_CURSOR_LEFT;
-		case 39:
-			return jevent::JKS_CURSOR_RIGHT;
-		case 38:  
-			return jevent::JKS_CURSOR_UP;
-		case 40:
-			return jevent::JKS_CURSOR_DOWN;
-		// case 16777222:  
-		//	return jevent::JKS_INSERT;
-		// case 16777232:     
-		//	return jevent::JKS_HOME;
-		// case 16777233:
-		//	return jevent::JKS_END;
-		// case 16777238:
-		//	return jevent::JKS_PAGE_UP;
-		// case 16777239:
-		//	return jevent::JKS_PAGE_DOWN;
-		// case ALLEGRO_KEY_PRINTSCREEN:   
-		//	return jevent::JKS_PRINT;
-		// case 16777224:
-		//	return jevent::JKS_PAUSE;
-		// case ALLEGRO_KEY_RED:
-		// 	return jevent::JKS_RED;
-		// case ALLEGRO_KEY_GREEN:
-		// 	return jevent::JKS_GREEN;
-		// case ALLEGRO_KEY_YELLOW:
-		// 	return jevent::JKS_YELLOW;
-		// case ALLEGRO_KEY_BLUE:
-		// 	return jevent::JKS_BLUE;
-		case 65470:
-		 	return jevent::JKS_F1;
-		case 65471:
-		 	return jevent::JKS_F2;
-		case 65472:
-			return jevent::JKS_F3;
-		case 65473:
-			return jevent::JKS_F4;
-		case 65474:
-			return jevent::JKS_F5;
-		case 65475:
-			return jevent::JKS_F6;
-		case 65476:
-		 	return jevent::JKS_F7;
-		case 65477:
-			return jevent::JKS_F8;
-		case 65478:
-			return jevent::JKS_F9;
-		case 65479:
-		 	return jevent::JKS_F10;
-		case 65480:
-			return jevent::JKS_F11;
-		case 65481:
-		 	return jevent::JKS_F12;
+  switch (symbol) {
+    case 13:
+      return jevent::JKS_ENTER; // jevent::JKS_RETURN;
+    case 8:
+      return jevent::JKS_BACKSPACE;
+    case 9:
+      return jevent::JKS_TAB;
+    // case ALLEGRO_KEY_CANCEL:
+    //  return jevent::JKS_CANCEL;
+    case 27:
+      return jevent::JKS_ESCAPE;
+    case 32:
+      return jevent::JKS_SPACE;
+    // case 39:
+    //  return (capital == true)?jevent::JKS_QUOTATION:jevent::JKS_APOSTHROFE;
+    case 44:   
+      return (capital == true)?jevent::JKS_LESS_THAN_SIGN:jevent::JKS_COMMA;
+    case 45:
+      return (capital == true)?jevent::JKS_UNDERSCORE:jevent::JKS_MINUS_SIGN;
+    case 46:  
+      return (capital == true)?jevent::JKS_GREATER_THAN_SIGN:jevent::JKS_PERIOD;
+    case 47:
+      return (capital == true)?jevent::JKS_QUESTION_MARK:jevent::JKS_SLASH;
+    case 48:     
+      return (capital == true)?jevent::JKS_PARENTHESIS_RIGHT:jevent::JKS_0;
+    case 49:
+      return (capital == true)?jevent::JKS_EXCLAMATION_MARK:jevent::JKS_1;
+    case 50:
+      return (capital == true)?jevent::JKS_AT:jevent::JKS_2;
+    case 51:
+      return (capital == true)?jevent::JKS_NUMBER_SIGN:jevent::JKS_3;
+    case 52:
+      return (capital == true)?jevent::JKS_DOLLAR_SIGN:jevent::JKS_4;
+    case 53:
+      return (capital == true)?jevent::JKS_PERCENT_SIGN:jevent::JKS_5;
+    case 54:
+      return (capital == true)?jevent::JKS_CIRCUMFLEX_ACCENT:jevent::JKS_6;
+    case 55:
+      return (capital == true)?jevent::JKS_AMPERSAND:jevent::JKS_7;
+    case 56:
+      return (capital == true)?jevent::JKS_STAR:jevent::JKS_8;
+    case 57:
+      return (capital == true)?jevent::JKS_PARENTHESIS_LEFT:jevent::JKS_9;
+    case 59:
+      return (capital == true)?jevent::JKS_COLON:jevent::JKS_SEMICOLON;
+    case 61: 
+      return (capital == true)?jevent::JKS_PLUS_SIGN:jevent::JKS_EQUALS_SIGN;
+    case 65:
+      return (capital == true)?jevent::JKS_A:jevent::JKS_a;
+    case 66:
+      return (capital == true)?jevent::JKS_B:jevent::JKS_b;
+    case 67:
+      return (capital == true)?jevent::JKS_C:jevent::JKS_c;
+    case 68:
+      return (capital == true)?jevent::JKS_D:jevent::JKS_d;
+    case 69:
+      return (capital == true)?jevent::JKS_E:jevent::JKS_e;
+    case 70:
+      return (capital == true)?jevent::JKS_F:jevent::JKS_f;
+    case 71:
+      return (capital == true)?jevent::JKS_G:jevent::JKS_g;
+    case 72:
+      return (capital == true)?jevent::JKS_H:jevent::JKS_h;
+    case 73:
+      return (capital == true)?jevent::JKS_I:jevent::JKS_i;
+    case 74:
+      return (capital == true)?jevent::JKS_J:jevent::JKS_j;
+    case 75:
+      return (capital == true)?jevent::JKS_K:jevent::JKS_k;
+    case 76:
+      return (capital == true)?jevent::JKS_L:jevent::JKS_l;
+    case 77:
+      return (capital == true)?jevent::JKS_M:jevent::JKS_m;
+    case 78:
+      return (capital == true)?jevent::JKS_N:jevent::JKS_n;
+    case 79:
+      return (capital == true)?jevent::JKS_O:jevent::JKS_o;
+    case 80:
+      return (capital == true)?jevent::JKS_P:jevent::JKS_p;
+    case 81:
+      return (capital == true)?jevent::JKS_Q:jevent::JKS_q;
+    case 82:
+      return (capital == true)?jevent::JKS_R:jevent::JKS_r;
+    case 83:
+      return (capital == true)?jevent::JKS_S:jevent::JKS_s;
+    case 84:
+      return (capital == true)?jevent::JKS_T:jevent::JKS_t;
+    case 85:
+      return (capital == true)?jevent::JKS_U:jevent::JKS_u;
+    case 86:
+      return (capital == true)?jevent::JKS_V:jevent::JKS_v;
+    case 87:
+      return (capital == true)?jevent::JKS_W:jevent::JKS_w;
+    case 88:
+      return (capital == true)?jevent::JKS_X:jevent::JKS_x;
+    case 89:
+      return (capital == true)?jevent::JKS_Y:jevent::JKS_y;
+    case 90:
+      return (capital == true)?jevent::JKS_Z:jevent::JKS_z;
+    case 91:
+      return (capital == true)?jevent::JKS_CURLY_BRACKET_LEFT:jevent::JKS_SQUARE_BRACKET_LEFT;
+    case 92:   
+      return (capital == true)?jevent::JKS_VERTICAL_BAR:jevent::JKS_BACKSLASH;
+    case 93:
+      return (capital == true)?jevent::JKS_CURLY_BRACKET_RIGHT:jevent::JKS_SQUARE_BRACKET_RIGHT;
+    case 96:
+      return (capital == true)?jevent::JKS_TILDE:jevent::JKS_GRAVE_ACCENT;
+    case 127:
+      return jevent::JKS_DELETE;
+    case 37:
+      return jevent::JKS_CURSOR_LEFT;
+    case 39:
+      return jevent::JKS_CURSOR_RIGHT;
+    case 38:  
+      return jevent::JKS_CURSOR_UP;
+    case 40:
+      return jevent::JKS_CURSOR_DOWN;
+    // case 16777222:  
+    //  return jevent::JKS_INSERT;
+    // case 16777232:     
+    //  return jevent::JKS_HOME;
+    // case 16777233:
+    //  return jevent::JKS_END;
+    // case 16777238:
+    //  return jevent::JKS_PAGE_UP;
+    // case 16777239:
+    //  return jevent::JKS_PAGE_DOWN;
+    // case ALLEGRO_KEY_PRINTSCREEN:   
+    //  return jevent::JKS_PRINT;
+    // case 16777224:
+    //  return jevent::JKS_PAUSE;
+    // case ALLEGRO_KEY_RED:
+    //  return jevent::JKS_RED;
+    // case ALLEGRO_KEY_GREEN:
+    //  return jevent::JKS_GREEN;
+    // case ALLEGRO_KEY_YELLOW:
+    //  return jevent::JKS_YELLOW;
+    // case ALLEGRO_KEY_BLUE:
+    //  return jevent::JKS_BLUE;
+    case 65470:
+      return jevent::JKS_F1;
+    case 65471:
+      return jevent::JKS_F2;
+    case 65472:
+      return jevent::JKS_F3;
+    case 65473:
+      return jevent::JKS_F4;
+    case 65474:
+      return jevent::JKS_F5;
+    case 65475:
+      return jevent::JKS_F6;
+    case 65476:
+      return jevent::JKS_F7;
+    case 65477:
+      return jevent::JKS_F8;
+    case 65478:
+      return jevent::JKS_F9;
+    case 65479:
+      return jevent::JKS_F10;
+    case 65480:
+      return jevent::JKS_F11;
+    case 65481:
+      return jevent::JKS_F12;
     case 16:
-		 	return jevent::JKS_SHIFT;
-		case 17:
-		 	return jevent::JKS_CONTROL;
-		// case 16777251:
-		// 	return jevent::JKS_ALT;
-		// case ALLEGRO_KEY_ALTGR:
-		//	return jevent::JKS_ALTGR;
-		// case ALLEGRO_KEY_LMETA:
-		// case ALLEGRO_KEY_RMETA:
-		// 	return jevent::JKS_META;
-		// case ALLEGRO_KEY_LSUPER:
-		// case ALLEGRO_KEY_RSUPER:
-		// 	return jevent::JKS_SUPER;
-		// case ALLEGRO_KEY_HYPER:
-		// 	return jevent::JKS_HYPER;
-		default: 
-			break;
-	}
+      return jevent::JKS_SHIFT;
+    case 17:
+      return jevent::JKS_CONTROL;
+    // case 16777251:
+    //  return jevent::JKS_ALT;
+    // case ALLEGRO_KEY_ALTGR:
+    //  return jevent::JKS_ALTGR;
+    // case ALLEGRO_KEY_LMETA:
+    // case ALLEGRO_KEY_RMETA:
+    //  return jevent::JKS_META;
+    // case ALLEGRO_KEY_LSUPER:
+    // case ALLEGRO_KEY_RSUPER:
+    //  return jevent::JKS_SUPER;
+    // case ALLEGRO_KEY_HYPER:
+    //  return jevent::JKS_HYPER;
+    default: 
+      break;
+  }
 
-	return jevent::JKS_UNKNOWN;
+  return jevent::JKS_UNKNOWN;
 }
 
 NativeApplication::NativeApplication():
-	jgui::Application()
+  jgui::Application()
 {
-	jcommon::Object::SetClassName("jgui::NativeApplication");
+  jcommon::Object::SetClassName("jgui::NativeApplication");
 }
 
 NativeApplication::~NativeApplication()
@@ -263,8 +267,8 @@ void NativeApplication::InternalInit(int argc, char **argv)
   nana::screen s = nana::screen();
   const ::nana::rectangle r = s.get_primary().workarea();
 
-	sg_screen.width = r.width;
-	sg_screen.height = r.height;
+  sg_screen.width = r.width;
+  sg_screen.height = r.height;
   
   sg_quitting = false;
 }
@@ -280,7 +284,7 @@ static void PaintThread(NativeApplication *app)
     
     dw->update();
 
-    sg_paint_condition.wait_for(lock, std::chrono::milliseconds(100));
+    sg_paint_condition.wait_for(lock, std::chrono::milliseconds(1000));
   }
 }
 
@@ -503,18 +507,18 @@ static void paint_callback(const nana::paint::graphics& graph)
 }
 
 NativeWindow::NativeWindow(int x, int y, int width, int height):
-	jgui::Window(dynamic_cast<Window *>(this))
+  jgui::Window(dynamic_cast<Window *>(this))
 {
-	jcommon::Object::SetClassName("jgui::NativeWindow");
+  jcommon::Object::SetClassName("jgui::NativeWindow");
 
-	if (fm != nullptr) {
-		throw jexception::RuntimeException("Cannot create more than one window");
+  if (fm != nullptr) {
+    throw jexception::RuntimeException("Cannot create more than one window");
   }
 
   sg_jgui_icon = new BufferedImage(_DATA_PREFIX"/images/small-gnu.png");
 
-	sg_mouse_x = 0;
-	sg_mouse_y = 0;
+  sg_mouse_x = 0;
+  sg_mouse_y = 0;
 
 
   fm = new nana::form{nana::API::make_center(width, height)};
@@ -560,6 +564,20 @@ void NativeWindow::Repaint(Component *cmp)
 
 void NativeWindow::ToggleFullScreen()
 {
+  if (sg_fullscreen == false) {
+    sg_last_bounds = GetBounds();
+
+    fm->size({(nana::size::value_type)sg_screen.width, (nana::size::value_type)sg_screen.height});
+    fm->move({0, 0});
+
+    sg_fullscreen = true;
+  } else {
+    fm->size({(nana::size::value_type)sg_last_bounds.width, (nana::size::value_type)sg_last_bounds.height});
+    fm->move({sg_last_bounds.x, sg_last_bounds.y});
+
+    sg_fullscreen = false;
+  }
+
 }
 
 void NativeWindow::SetParent(jgui::Container *c)
@@ -584,7 +602,7 @@ void NativeWindow::SetTitle(std::string title)
 
 std::string NativeWindow::GetTitle()
 {
-	return fm->caption();
+  return fm->caption();
 }
 
 void NativeWindow::SetOpacity(float opacity)
@@ -593,7 +611,7 @@ void NativeWindow::SetOpacity(float opacity)
 
 float NativeWindow::GetOpacity()
 {
-	return 1.0;
+  return 1.0;
 }
 
 void NativeWindow::SetUndecorated(bool undecorated)
@@ -613,16 +631,19 @@ void NativeWindow::SetBounds(int x, int y, int width, int height)
 
 jgui::jregion_t NativeWindow::GetBounds()
 {
-	jgui::jregion_t t {0, 0, 0, 0};
+  jgui::jregion_t t {0, 0, 0, 0};
  
   nana::size s = fm->size();
+  nana::point p = fm->pos();
 
+  t.x = p.x;
+  t.y = p.y;
   t.width = s.width;
   t.height = s.height;
 
-	return t;
+  return t;
 }
-		
+    
 void NativeWindow::SetResizable(bool resizable)
 {
 }
@@ -638,12 +659,12 @@ void NativeWindow::SetCursorLocation(int x, int y)
 
 jpoint_t NativeWindow::GetCursorLocation()
 {
-	jpoint_t p {
+  jpoint_t p {
     .x = sg_mouse_x, 
     .y = sg_mouse_y
   };
 
-	return p;
+  return p;
 }
 
 void NativeWindow::SetVisible(bool visible)
@@ -671,7 +692,7 @@ void NativeWindow::SetCursorEnabled(bool enabled)
 
 bool NativeWindow::IsCursorEnabled()
 {
-	return true;
+  return true;
 }
 
 void NativeWindow::SetCursor(jcursor_style_t style)
@@ -706,7 +727,7 @@ void NativeWindow::SetCursor(jcursor_style_t style)
   }
 
   fm->cursor(type);
-	
+  
   sg_jgui_cursor = style;
 }
 
@@ -716,12 +737,12 @@ void NativeWindow::SetCursor(Image *shape, int hotx, int hoty)
 
 void NativeWindow::SetRotation(jwindow_rotation_t t)
 {
-	// TODO::
+  // TODO::
 }
 
 jwindow_rotation_t NativeWindow::GetRotation()
 {
-	return jgui::JWR_NONE;
+  return jgui::JWR_NONE;
 }
 
 void NativeWindow::SetIcon(jgui::Image *image)
