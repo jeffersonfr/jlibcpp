fa939c5eeee39095af6cc197286ecbebd3392d72
Jeff Ferr <jeffersonfr@gmail.com>
2019-05-28 02:45:41 -0300

    Adding jgui/tests/dummy.cpp :)

---
+++ jgui/tests/dummy.cpp
@@ -0,0 +1,34 @@
+#include "jgui/japplication.h"
+#include "jgui/jwindow.h"
+
+class Dummy : public jgui::Window {
+
+  public:
+    Dummy():
+      jgui::Window(100, 100)
+    {
+    }
+
+    virtual ~Dummy()
+    {
+    }
+
+    virtual void Paint(jgui::Graphics *g) 
+    {
+			g->SetColor(jgui::Color::Red);
+			g->FillRectangle({0, 0, GetSize()});
+    }
+
+};
+
+int main(int argc, char *argv[])
+{
+  jgui::Application::Init(argc, argv);
+
+  Dummy app;
+
+  jgui::Application::Loop();
+
+  return 0;
+}
+
